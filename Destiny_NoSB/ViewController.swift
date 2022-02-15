fda434cf0d44aece458dd04826eb4f8146b2d9a3
Raúl Pavón <raul.pavon.toral@gmail.com>
2022-02-15 12:43:02 -0600

    Initial Commit

---
+++ Destiny_NoSB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destiny_NoSB
+//
+//  Created by Raúl Pavón on 15/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
